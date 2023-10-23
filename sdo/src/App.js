c661bc7414749ff9d9573a100fb9c1b69dd03514
DailyPepper <danielkudinov17@gmail.com>
2023-10-24 00:03:27 +0300

    commit-route

--- sdo/src/App.js
+++ sdo/src/App.js
@@ -2,13 +2,23 @@ import Header from "./components/Header/Header";
 import React from "react";
 import Main from "./components/Main/Main";
 import Footer from "./components/Footer/Footer";
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <Header/>
-      <Main/>
-      <Footer/>
+      <Router>
+        <Header/>
+        <Routes>
+          <Route path="" element = {<Main/>}></Route>
+
+{/* Прописать все остальные route по тому же принципу */}
+
+          
+        </Routes>
+        <Footer/>  
+      </Router>
+      
     </div>
   );
 }
