9aac84934400e68d43b0d71a36fed420ebfdd946
DailyPepper <danielkudinov17@gmail.com>
2023-11-13 00:31:05 +0300

    add new page prepodLav

--- sdo/src/components/BreadCrumbs.jsx
+++ sdo/src/components/BreadCrumbs.jsx
@@ -2,11 +2,18 @@ import React from "react";
 import { Link } from "react-router-dom";
 import { useLocation } from "react-router-dom";
 
-
 const Bread = () => {
-    return ( 
-        <nav>
+    const location = useLocation();
+    const pathNames = location.pathname.split('/').filter((path)=> path !== '');
 
+    return ( 
+        <nav aria-label="breadcrumbs">
+            <ul className="breadcrumbs">
+                <li>
+                    <Link to='/'>Home</Link>
+                </li>
+                
+            </ul>
         </nav>
      );
 }
---
+++ sdo/src/pages/PrepodRedLab.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import Header from "../components/Header/Header";
+import Footer from "../components/Footer/Footer";
+
+const PrepodRedLab = () => {
+    return ( 
+        <>
+        <Header></Header>
+        <section>
+            <div>
+                <h3>
+                    Вы находитесть в режиме редактирования: 
+                    Объектно-ориентированное программирование 
+                    Лабораторная работа №1 
+                    “Создание программы с использованием классов”
+                </h3>
+            </div>
+            <div>
+                
+            </div> 
+        </section>
+        
+        <Footer></Footer>
+        </>
+     );
+}
+ 
+export default PrepodRedL
