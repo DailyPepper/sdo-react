d81005b9f496e3048ac5adcdeb53df9824cfebc3
DailyPepper <danielkudinov17@gmail.com>
2023-11-20 01:38:25 +0300

    add new components in prepodredlab

--- sdo/src/pages/PrepodRedLab.jsx
+++ sdo/src/pages/PrepodRedLab.jsx
@@ -6,33 +6,64 @@ const PrepodRedLab = () => {
     return ( 
         <>
         <Header></Header>
-        <section>
-            <div className="editing__block">
-                <div className="editing__block-name">
-                    <h3 className="editing__block-title">
+        <section className="section__editing">
+            <div className="editing__blocks">
+                <div className="editing__block">
+                    <h3 className="editing__block-heading">
                         Вы находитесть в режиме редактирования: 
                         Объектно-ориентированное программирование 
                         Лабораторная работа №1 
                         “Создание программы с использованием классов”
                     </h3>
                 </div>
-                <div>
-                    <h3>
-                        Формат ввода данных:
-                    </h3>
-                    <p>
-                        Вы можете пояснить, как будет происходить ввод данных
-                    </p>
-                    <input type="text" />
+                <div className="editing__block">
+                    <div className="editing__block-Two">
+                        <h3 className="editing__block-title">
+                            Формат ввода данных:
+                        </h3>
+                            <p className="editing__block-text">
+                                Вы можете пояснить, как будет происходить ввод данных
+                            </p>
+                            <input className="editing__block-input" type="text" />   
+                    </div>
                 </div> 
-                {/* <div>
-                    <h3>
+                <div className="editing__container">
+                    <div className="editing__block-Two">
+                        <h3 className="editing__block-title"> 
+                            Проверка длины:
+                        </h3>
+                        <div className="editing__block-inp">
+                            <p className="editing__block-title">
+                                Максимальное количество символов:
+                            </p> 
+                            <input type="text" className="some-input"/>   
+                        </div>
+                        <div className="editing__block-inp">
+                            <p className="editing__block-title">
+                                Максимальное количество строк:
+                            </p>    
+                            <input type="text" className="some-input"/>   
+                        </div>
+                        
+                        <div className="editing__block-bth">
+                            <button className="editing__block-bthAdd">
+                                Добавить проверку длины
+                            </button>
+                            <button className="editing__block-btnDel">
+                                Удалить
+                            </button>
+                        </div>   
+                    </div>
+                    
+                </div>   
+                <div className="editing__container">
+                    <h3 className="editing__block-title"> 
                         Проверка длины:
                     </h3>
-                    <p>
+                    <p className="editing__block-title">
                         Максимальное количество символов:
                     </p>
-                    <p>
+                    <p className="editing__block-title">
                         Максимальное количество строк:
                     </p>
                     <div>
@@ -43,12 +74,12 @@ const PrepodRedLab = () => {
                             Удалить
                         </button>
                     </div>
-                </div>    */}
+                </div> 
             </div>
             
         </section>
         
-        <Footer></Footer>
+        {/* <Footer></Footer> */}
         </>
      );
 }
