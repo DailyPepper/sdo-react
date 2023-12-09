157f170d610bbc36e593a172f1962377316b0ecb
DailyPepper <danielkudinov17@gmail.com>
2023-12-10 02:00:30 +0300

    add folder

--- sdo/src/pages/Auto/Auto.jsx
+++ sdo/src/pages/Auto/Auto.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import "../styles/style.css"
-import Header from '../components/Header/Header';
-import Footer from '../components/Footer/Footer';
+import Header from '../../components/Header/Header';
+import Footer from '../../components/Footer/Footer';
 
 const Auto = () => {
   const [username, setUsername] = useState('');
--- sdo/src/pages/Laboratory/Laboratory.jsx
+++ sdo/src/pages/Laboratory/Laboratory.jsx
@@ -1,6 +1,6 @@
 import { Link } from "react-router-dom";
 import React, { useState } from "react";
-import Header from "../components/Header/Header";
+import Header from "../../components/Header/Header";
 // import Footer from "../components/Footer/Footer";
 import "../styles/style.css";
 
